e4167d9ac569a1326369523d441134e319e33ceb
JeicksonT1000 <j13buenosoldier@gmail.com>
2021-08-21 12:05:39 -0300

    compoemente nav criadoo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,7 +26,10 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+
+//Compomentes do sistema
 import { NavComponent } from './components/nav/nav.component';
+
 @NgModule({
   declarations: [
     AppComponent,
