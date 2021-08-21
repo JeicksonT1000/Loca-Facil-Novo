342ae278097df44094109cbcdd0dbb84403e5f34
JeicksonT1000 <j13buenosoldier@gmail.com>
2021-08-21 19:49:42 -0300

    html e css para client-list

--- src/app/components/cliente/cliente-list/cliente-list.component.ts
+++ src/app/components/cliente/cliente-list/cliente-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Cliente } from 'src/app/models/cliente';
 
 @Component({
   selector: 'app-cliente-list',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClienteListComponent implements OnInit {
 
+  ELEMENT_DATA: Cliente[] = [
+    {
+      tamanho: 'caçamba grande',
+      preco: '500',
+      dataRecolhimento: '15/08/2021',
+      situacao: 'Em uso'
+    }
+  ]
+  
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acaoRenovar', 'acaoRecolhimento'];
+  dataSource = new MatTableDataSource<Cliente>(this.ELEMENT_DATA);
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.dataSource);
+    
   }
 
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
