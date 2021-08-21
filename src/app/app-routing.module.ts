cdabbaffa7c64641d5d9b5394c437db3a2dc49b5
JeicksonT1000 <j13buenosoldier@gmail.com>
2021-08-21 18:25:51 -0300

    rota criado para client-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { NavComponent } from './components/nav/nav.component';
 import { SolicitacaoComponent } from './components/solicitacao/solicitacao.component';
 
 const routes: Routes = [
   {
     path: '', component: NavComponent, children: [
-      { path: 'solicitacao', component: SolicitacaoComponent }
+      { path: 'solicitacao', component: SolicitacaoComponent },
+      { path: 'clientManager', component: ClienteListComponent }
     ]
   }
 ];
