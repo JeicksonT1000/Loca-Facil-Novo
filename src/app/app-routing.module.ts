177872fecd19f51115b39810ef290731ffef2b79
JeicksonT1000 <j13buenosoldier@gmail.com>
2021-08-21 15:27:05 -0300

    rota para componente solicitacao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
+import { SolicitacaoComponent } from './components/solicitacao/solicitacao.component';
 
 const routes: Routes = [
   {
-    path: '', component: NavComponent
+    path: '', component: NavComponent, children: [
+      { path: 'solicitacao', component: SolicitacaoComponent }
+    ]
   }
 ];
 
