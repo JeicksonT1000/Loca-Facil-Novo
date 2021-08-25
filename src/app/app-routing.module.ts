911118462c8b49d957651f6a8dcb729ab368e374
JeicksonT1000 <j13buenosoldier@gmail.com>
2021-08-24 23:23:02 -0300

    html e css para compoment redefinir senha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
+import { RedefinirComponent } from './components/redefinir/redefinir.component';
 import { SolicitacaoComponent } from './components/solicitacao/solicitacao.component';
 
 const routes: Routes = [
   
   { path: 'login', component: LoginComponent },
   {path: 'cadastro', component: CadastroComponent},
+  { path: 'redefinirSenha', component: RedefinirComponent },
 
   {
     path: '', component: NavComponent, canActivate: [AuthGuard], children: [
